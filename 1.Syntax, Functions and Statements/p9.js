da20177c4d4c61c293f5dd4561973ac41f613c45
Radoslav Bekyarov <rbekyarov82@gmail.com>
2022-04-11 22:20:46 +0300

    p9

---
+++ 1.Syntax, Functions and Statements/p9.js
@@ -0,0 +1,10 @@
+function solve(countOfStar){
+    for (let index = 0; index < countOfStar; index++) {
+        console.log('* '.repeat(countOfStar));
+        
+    }
+    
+}
+//solve(1);
+//solve(2);
+solve(5);
