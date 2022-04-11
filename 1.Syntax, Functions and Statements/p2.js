7544c06edb5b6ec8324e7311eb033b21417e7672
Radoslav Bekyarov <rbekyarov82@gmail.com>
2022-04-11 11:05:25 +0300

    first problem solved

---
+++ 1.Syntax, Functions and Statements/p2.js
@@ -0,0 +1,8 @@
+function solve (input1,input2,input3){
+    let sum_length = input1.length+input2.length+input3.length;
+    console.log(sum_length);
+    let avg = Math.floor(sum_length/3);
+    console.log(avg);
+}
+//solve('chocolate', 'ice cream', 'cake');
+solve('pasta', '5', '22.3');
---
+++ 1.Syntax, Functions and Statements/p3.js
@@ -0,0 +1,8 @@
+function solve(num1,num2,num3){
+    let max_number = Math.max(num1,num2,num3);
+
+console.log(`The largest number is ${max_number}.`)
+
+}
+//solve(5, -3, 16);
+solve(-3, -5, -22.5);
