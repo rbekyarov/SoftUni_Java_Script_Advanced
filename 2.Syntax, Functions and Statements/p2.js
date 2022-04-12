b77d689e1eb131252085f19ff5792137d5d6165a
Radoslav Bekyarov <rbekyarov82@gmail.com>
2022-04-12 10:31:26 +0300

    p2

---
+++ 2.Syntax, Functions and Statements/p2.js
@@ -0,0 +1,15 @@
+function solve (num1,num2){
+    let min = Math.min(num1,num2);
+    for (let i = min; i > 0; i--) {
+        
+        let result1 = Number(num1)/i;        
+        let result2 = Number(num2)/i;
+        
+        if(Number.isInteger(result1)&&Number.isInteger(result2)){
+            console.log(i);
+            break;
+        }        
+    }
+}
+//solve(15,5);
+solve(2154, 458);
