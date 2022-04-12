97ed18cb61fc1ce26081c1316188218e9d032d29
Radoslav Bekyarov <rbekyarov82@gmail.com>
2022-04-12 15:55:32 +0300

    p5

---
+++ 2.Syntax, Functions and Statements/p5.js
@@ -0,0 +1,25 @@
+function solve (steps ,lengthFootprint ,speed){
+    //време?
+    //разстояние?
+    let distance = steps*lengthFootprint; //разстояние в metri
+    let restCount = distance/500;
+    let timeRest = parseInt(restCount, 10);
+    let seconds = (distance/speed/1000*60+timeRest)*60; //време в sekundi
+    
+    convertSeconds(seconds);
+    
+    
+    }
+    
+    
+
+function convertSeconds(seconds) {
+    var convert = function(x) { return (x < 10) ? "0"+x : x; }
+    let result = convert(parseInt(seconds / (60*60))) + ":" +
+           convert(parseInt(seconds / 60 % 60)) + ":" +
+           Math.round(convert(seconds % 60));
+
+           console.log(result);
+  }
+solve(4000, 0.60, 5);
+solve(2564, 0.70, 5.5);
