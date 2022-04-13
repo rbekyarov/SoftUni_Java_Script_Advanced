b43e8162fd44d413c7c3e5317515bcdb746040b2
Radoslav Bekyarov <rbekyarov82@gmail.com>
2022-04-13 21:16:22 +0300

    p2

---
+++ 3.Arrays and Nested Arrays/p2.js
@@ -0,0 +1,20 @@
+function solve(n, k) {
+    
+    let arr = [1];
+    
+    for (let i = 0; i < n-1; i++) {
+        let sum=0;
+        for (let j = 0; j < k; j++) {
+            if(j >= arr.length){
+                continue;
+            }
+            sum += arr[arr.length - 1 - j];
+
+        }
+        arr.push(sum);
+    }
+    console.log(arr);
+}
+
+solve(6, 3);
+solve(8, 2);
