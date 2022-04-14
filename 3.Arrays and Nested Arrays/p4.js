341e1b1260e8d51ec413cc8ef8879403f7810e2e
Radoslav Bekyarov <rbekyarov82@gmail.com>
2022-04-14 10:00:06 +0300

    p4

--- 3.Arrays and Nested Arrays/p4.js
+++ 3.Arrays and Nested Arrays/p4.js
@@ -1,4 +1,4 @@
-function solve(arr) {
+function solve1(arr) {
     let arrResult = [];
     let length = arr.length;
 
@@ -14,5 +14,23 @@ function solve(arr) {
     }
 }
 
-solve([7, -2, 8, 9]);
-solve([3, -2, 0, -1]);
+function solve2(arr1) {
+    
+    arrResult  = [];
+
+    for (let element of arr1) {
+        if (element < 0) {
+            arrResult.unshift(element);
+        } else {
+            arrResult.push(element);
+        }
+    }
+    console.log(arrResult.join('\n'));
+}
+
+
+solve1([7, -2, 8, 9]);
+solve1([3, -2, 0, -1]);
+
+solve2([7, -2, 8, 9]);
+solve2([3, -2, 0, -1]);
