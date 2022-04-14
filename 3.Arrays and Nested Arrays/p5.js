f914eeb03aafe8cc9fe1963e3f21216c1f9a7b3b
Radoslav Bekyarov <rbekyarov82@gmail.com>
2022-04-14 10:19:00 +0300

    p5

---
+++ 3.Arrays and Nested Arrays/p5.js
@@ -0,0 +1,15 @@
+function solve(arr) {
+    let arrResult = [];
+    
+
+    for (let i = 0; i < 2; i++) {
+        let currentMinElement = Math.min.apply(Math, arr) //най - малкото число в масива
+        let index = arr.indexOf(currentMinElement); //вземам индекса на най малкото число
+        arr.splice(index, 1); //изтривам индекса
+        arrResult.push(currentMinElement); //добавям най-малкото число в нов масив
+
+    }
+    console.log(arrResult.join(' '));
+}
+solve([30, 15, 50, 5]);
+solve([3, 0, 10, 4, 7, 3]);
