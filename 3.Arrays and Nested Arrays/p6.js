ad2eeda023914502825ed513ad9e92a975cd849d
Radoslav Bekyarov <rbekyarov82@gmail.com>
2022-04-14 10:56:19 +0300

    p6

--- 3.Arrays and Nested Arrays/p6.js
+++ 3.Arrays and Nested Arrays/p6.js
@@ -1,9 +1,6 @@
 function solve(arr) {
-    
     arr.sort((a, b) => a - b);
-    
     let arrLength = (arr.length - 1) / 2;
-    
     arrLength = Math.round(arrLength);
     
     let resultArr = [];
@@ -11,12 +8,9 @@ function solve(arr) {
     for (let i = arrLength; i < arr.length; i++) {
         let element = arr[i];
         resultArr.push(element);
-
-
     }
 
     console.log(resultArr);
-
 }
 solve([4, 7, 2, 5]);
 solve([3, 19, 14, 7, 2, 19, 6]);
