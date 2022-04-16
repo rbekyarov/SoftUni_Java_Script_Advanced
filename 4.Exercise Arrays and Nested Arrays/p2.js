f51dbb42fdf44b881ecadec87cd91d901357e133
Radoslav Bekyarov <rbekyarov82@gmail.com>
2022-04-16 09:07:42 +0300

    p2

---
+++ 4.Exercise Arrays and Nested Arrays/p2.js
@@ -0,0 +1,31 @@
+function solve(array, n){
+     let arr = [];
+    
+    for (let index = 0; index < array.length; index+=Number(n)) {
+        arr.push(array[index]);
+        
+    }
+    console.log(arr);
+}
+
+solve(['5', 
+'20', 
+'31', 
+'4', 
+'20'], 
+2
+);
+
+solve(['dsa',
+'asd', 
+'test', 
+'tset'], 
+2
+);
+solve(['1', 
+'2',
+'3', 
+'4', 
+'5'], 
+6
+);
