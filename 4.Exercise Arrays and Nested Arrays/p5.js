225e9ced06e6f69af5bd4f75dc378516814c65b3
Radoslav Bekyarov <rbekyarov82@gmail.com>
2022-04-16 21:45:30 +0300

    p5

---
+++ 4.Exercise Arrays and Nested Arrays/p5.js
@@ -0,0 +1,34 @@
+function solve(input) {
+    
+    let currentMaxNumber = input[0];
+    let result = [];
+    
+    for (let currentNumber of input) {
+        if (currentNumber >= currentMaxNumber) {
+            result.push(currentNumber);
+            currentMaxNumber = currentNumber;
+        }
+    }
+    console.log(result);
+    return result;
+    
+}
+
+solve([1,
+    3,
+    8,
+    4,
+    10,
+    12,
+    3,
+    2,
+    24
+]);
+
+solve([20, 
+    3, 
+    2, 
+    15,
+    6, 
+    1]
+    );
