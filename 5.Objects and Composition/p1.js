db1ffcc648d2cee45c85d621e23fcdbb5d97d73e
Radoslav Bekyarov <rbekyarov82@gmail.com>
2022-04-17 13:20:54 +0300

    p1

---
+++ 5.Objects and Composition/p1.js
@@ -0,0 +1,16 @@
+function solve(name,population,treasury){
+
+    let city = {
+        name:name,
+        population:population,
+        treasury:treasury
+    };
+    //console.log(city);
+    console.log(city.population);
+    return city;
+   
+}
+solve('Tortuga',
+7000,
+15000
+);
