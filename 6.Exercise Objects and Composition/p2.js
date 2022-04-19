e84b000aaf03a8cf08bad2e68294b070344171dd
Radoslav Bekyarov <rbekyarov82@gmail.com>
2022-04-19 09:12:17 +0300

    p2

---
+++ 6.Exercise Objects and Composition/p2.js
@@ -0,0 +1,13 @@
+function solve(worker){
+ if(worker.dizziness){
+    worker.levelOfHydrated +=worker.weight*worker.experience/10;
+    worker.dizziness = false; 
+ }
+return worker;
+};
+
+solve({ weight: 80,
+    experience: 1,
+    levelOfHydrated: 0,
+    dizziness: true }
+  );
