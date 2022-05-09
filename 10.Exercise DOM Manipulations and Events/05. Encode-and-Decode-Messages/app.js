3222d50768206d130888c5ebe1c6904b96f2a596
Radoslav Bekyarov <rbekyarov82@gmail.com>
2022-05-09 22:42:53 +0300

    sde

--- 10.Exercise DOM Manipulations and Events/05. Encode-and-Decode-Messages/app.js
+++ 10.Exercise DOM Manipulations and Events/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,35 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+
+    [buttonEncode,buttonDecode] = Array.from(document.getElementsByTagName('button'));
+    buttonEncode.addEventListener('click', encode);
+    buttonDecode.addEventListener('click', decode);
+
+    function encode(){
+        let result='';
+        [message, receivedMessage] =Array.from(document.getElementsByTagName('textarea'));
+        message = message.value.toString();
+        for (let i = 0; i <message.length ; i++) {
+            let currentChar = Number(message.charCodeAt(i))+1;
+            let char = String.fromCharCode(currentChar);
+            result += char;
+        }
+        receivedMessage.value = result;
+        let arr =Array.from(document.getElementsByTagName('textarea'));
+        console.log(arr)
+        arr[0].value = '';
+    }
+
+    function decode(){
+        let result='';
+        [message, receivedMessage] =Array.from(document.getElementsByTagName('textarea'));
+        receivedMessage = receivedMessage.value;
+        for (let i = 0; i <receivedMessage.length ; i++) {
+            let currentChar = Number(receivedMessage.charCodeAt(i))-1;
+            let char = String.fromCharCode(currentChar);
+            result += char;
+        }
+        let arr =Array.from(document.getElementsByTagName('textarea'));
+        arr[1].value = result;
+
+    }
 }
--- 10.Exercise DOM Manipulations and Events/07. Distance-Converter/distanceConverter.js
+++ 10.Exercise DOM Manipulations and Events/07. Distance-Converter/distanceConverter.js
@@ -1,3 +1,70 @@
 function attachEventsListeners() {
-    console.log('TODO:...');
+let input = document.getElementById('inputDistance');
+
+let output = document.getElementById('outputDistance');
+let inputUnit = document.getElementById('inputUnits');
+let outputUnit = document.getElementById('outputUnits');
+let btn = document.getElementById('convert');
+btn.addEventListener('click', calculate);
+
+function calculate (){
+
+    let convert = Number(input.value);
+    let result = 0;
+    switch (inputUnit.value){
+        case 'km':
+            convert *=1000;
+            break;
+        case 'm':
+            convert =convert;
+            break;
+        case 'cm':
+            convert *=0.1;
+            break;
+        case 'mm':
+            convert *=0.001;
+            break;
+        case 'mi':
+            convert *=1609.34;
+            break;
+        case 'yrd':
+            convert *= 0.9144;
+            break;
+        case 'ft':
+            convert *= 0.3048
+            break;
+        case 'in':
+             convert *= 0.0254
+             break;
+    }
+    //console.log(convert)
+    switch (outputUnit.value){
+        case 'km':
+            result = convert /1000;
+            break;
+        case 'm':
+            result =convert;
+            break;
+        case 'cm':
+            result = convert /0.1;
+            break;
+        case 'mm':
+            result = convert /0.001;
+            break;
+        case 'mi':
+            result = convert /1609.34;
+            break;
+        case 'yrd':
+            result = convert /0.9144;
+            break;
+        case 'ft':
+            result = convert /0.3048
+            break;
+        case 'in':
+            result = convert /0.0254
+            break;
+    }
+    //console.log(result)
+    output.value = result;
+}
 }
