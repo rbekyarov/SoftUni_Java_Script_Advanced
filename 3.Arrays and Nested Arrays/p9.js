3de259ddbe57f87448ef44eb4f92fd5f5e1816fb
Radoslav Bekyarov <rbekyarov82@gmail.com>
2022-05-24 10:26:37 +0300

    sde

--- 3.Arrays and Nested Arrays/p9.js
+++ 3.Arrays and Nested Arrays/p9.js
@@ -1,5 +1,5 @@
 function solve(matrix) {
-    let maxNum = Number.MIN_VALUE;
+    let maxNum = Number.MIN_SAFE_INTEGER;
 
     for (let i = 0; i < matrix.length; i++) {
         for (let j = 0; j < matrix[i].length; j++) {
